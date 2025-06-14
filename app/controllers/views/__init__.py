631b2b17502ce5b4458c8b9f3e6eab6602170816
Caio Tenório <caiomatenorio@gmail.com>
2025-06-13 21:38:55 -0300

    refactor: remove redundant color_game and memory_game controllers from imports

--- app/controllers/views/__init__.py
+++ app/controllers/views/__init__.py
@@ -1,8 +1,1 @@
-from . import (
-    auth_controller,
-    color_game_controller,
-    home_page_controller,
-    memory_game_controller,
-    who_am_i_controller,
-    color_game_controller,
-)
+from . import auth_controller, game_controller, home_page_controller

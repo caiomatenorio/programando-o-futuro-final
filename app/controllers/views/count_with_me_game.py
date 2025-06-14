fb041695bc8cf875921fa2b1731519ebd27d8946
Caio Tenório <caiomatenorio@gmail.com>
2025-06-13 21:40:45 -0300

    refactor: reorganize game view controllers and remove unused files

--- app/controllers/views/count_with_me_game.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-
-from app.controllers.blueprints import views
-
-
-@views.get("/jogos/conte-comigo")
-def count_with_me_game():
-    return render_template("countWithMeGame.html")

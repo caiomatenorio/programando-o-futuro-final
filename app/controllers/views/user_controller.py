26838a7be5baf794e470624b19f7b53350fd0456
Caio Tenório <caiomatenorio@gmail.com>
2025-06-16 11:07:23 -0300

    refactor: update user account endpoints to use consistent path and add my_account view

---
+++ app/controllers/views/user_controller.py
@@ -0,0 +1,16 @@
+from flask import render_template, request
+
+from app.controllers.blueprints import views
+from app.services import user_service
+
+
+@views.get("/minha-conta")
+def my_account():
+    editar = request.args.get("editar")
+    user = user_service.get_current_user()
+    return render_template(
+        "my-account.html",
+        editar=editar,
+        name=user["name"],
+        email=user["email"],
+    )

d5c3df978a811ef7e8e05ce33d56ee71338f6956
Caio Tenório <caiomatenorio@gmail.com>
2025-06-17 18:37:05 -0300

    docs: add detailed docstrings for auth service functions to enhance clarity and maintainability

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,3 +1,11 @@
+"""
+# Auth Service Module
+
+This module provides authentication services, including user registration, login, logout, and
+session management. It interacts with the user service and session service to handle user
+authentication and session creation.
+"""
+
 from app.exceptions import SessionNotFoundException, UnauthorizedException
 from app.extensions import db
 
@@ -5,10 +13,31 @@ from . import session_service, user_service
 
 
 def register(name, email, password):
+    """
+    Register a new user with the provided name, email, and password. If the email is already in
+    use, an exception is raised.
+
+    :param name: The name of the user.
+    :param email: The email of the user.
+    :param password: The password for the user.
+    :return: None
+    :raises EmailAlreadyInUseException: If the email is already associated with an existing user.
+    """
+
     user_service.create_user(name, email, password)
 
 
 def login(email, password):
+    """
+    Login a user with the provided email and password. If the credentials are valid, a new session
+    is created, and a JWT is generated. If the credentials are invalid, an exception is raised.
+
+    :param email: The email of the user.
+    :param password: The password for the user.
+    :return: None
+    :raises InvalidCredentialsException: If the email or password is incorrect.
+    """
+
     with db.session.begin():
         user = user_service.validate_credentials(email, password, True)
         session = session_service.create_session(user.id)
@@ -17,6 +46,13 @@ def login(email, password):
 
 
 def logout():
+    """
+    Logout the current user by deleting their session. If no session is found, an exception is raised.
+
+    :return: None
+    :raises UnauthorizedException: If the user is not authenticated or the session does not exist.
+    """
+
     try:
         session_id = session_service.get_current_session_id()
         session_service.delete_session_by_id(session_id)
@@ -25,6 +61,14 @@ def logout():
 
 
 def is_authenticated():
+    """
+    Check if the current user is authenticated by validating the session. If the session is valid,
+    it returns True; otherwise, it returns False.
+
+    :return: True if the user is authenticated, False otherwise.
+    :raises UnauthorizedException: If the session is not valid or does not exist.
+    """
+
     try:
         session_service.validate_session()
         return True
